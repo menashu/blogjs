316f65cbcdaf939c60ac2319d9be59a80027f1d6
menashu <ashutoshmaherwal1@gmail.com>
2023-06-23 18:43:03 +0530

    new changes

--- main.tf
+++ main.tf
@@ -3,5 +3,12 @@ resource "aws_amplify_app" "nextjs" {
   repository = "https://github.com/menashu/blogjs"
 
   # GitHub personal access token
-  access_token = "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"
+  access_token = var.github_oauth_token
+  enable_branch_auto_build = true
+}
+
+
+variable "github_oauth_token" {
+  type        = string
+  description = "Value of the Personal Access Token from the GitHub"
 }
